0c0f1f39a6aa68e88f776f61d35c766f94ff784f
Megan Byers <megan@designelixir.studio>
2026-04-01 19:14:16 -0600

    add assets

--- tampermonkey-inject.js
+++ tampermonkey-inject.js
@@ -8,28 +8,14 @@
 // ==/UserScript==
 (function () {
     const CSS_URL = 'http://localhost:8080/custom.css';
-    const EVENTS_URL = 'http://localhost:8080/events';
 
     var styleEl = document.createElement('style');
     styleEl.id = '__local-css-override';
     document.head.appendChild(styleEl);
 
-    function loadCSS() {
-        GM_xmlhttpRequest({
-            method: 'GET',
-            url: CSS_URL + '?t=' + Date.now(),
-            onload: function(r) { styleEl.textContent = r.responseText; }
-        });
-    }
-
-    loadCSS();
-
     GM_xmlhttpRequest({
         method: 'GET',
-        url: EVENTS_URL,
-        headers: { 'Accept': 'text/event-stream' },
-        onreadystatechange: function(r) {
-            if (r.responseText.includes('data: reload')) loadCSS();
-        }
+        url: CSS_URL + '?t=' + Date.now(),
+        onload: function(r) { styleEl.textContent = r.responseText; }
     });
 })();
