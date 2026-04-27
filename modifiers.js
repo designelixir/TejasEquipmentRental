71369407a36a611b78ab71e321633b3f63b08f48
Megan Byers <megan@designelixir.studio>
2026-04-27 00:48:21 -0600

    test script

---
+++ modifiers.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    
+var selectedLocation = document.getElementsByClassName('company-logo')[0].innerHTML;
+console.log("selected location is ", selectedLocation)
+});
